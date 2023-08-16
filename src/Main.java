512ee18b3b5e3c190a795bf537715be76ed3493f
Ekaterina <shi.e.m@yandex.ru>
2023-08-16 17:59:53 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int amount = 23750;
+        int mile = 20;
+        int bonus;
+
+        System.out.println(bonus = amount / mile);
+    }
 }
